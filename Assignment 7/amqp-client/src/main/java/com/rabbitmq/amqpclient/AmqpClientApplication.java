7ad93a151bb5b41821b5f0d4c805fa4d4c731e24
Sunniva <69847932+h591296@users.noreply.github.com>
2023-10-22 13:44:02 +0200

    Add files via upload

---
+++ Assignment 7/amqp-client/src/main/java/com/rabbitmq/amqpclient/AmqpClientApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.amqpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqpClientApplication.class, args);
+	}
+
+}
